640d5a792529e5596d2ddb51f678a7aa9ab6e2d5
Luiz Morais <luiz.ado@hotmail.com>
2021-03-01 20:43:09 -0300

    Created a DefaultTheme file module to deal with theme declarations.

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    white: string;
+    background: string;
+    gray_line: string;
+    text: string;
+    title: string;
+    red: string;
+    green: string;
+    blue: string;
+    blue_dark: string;
+    blue_twitter: string;
+  }
+}
